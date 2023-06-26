db53191a18084cdd5e35a07d7ff52b4dafb7d389
Adam R <121298839+abtzpro@users.noreply.github.com>
2023-06-26 01:13:59 -0500

    Update DeepDivev1.py
    Version 1.2 update

--- DeepDivev1.py
+++ DeepDivev1.py
@@ -4,8 +4,24 @@ import psycopg2
 import json
 import socks
 import socket
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+import time
 
-# Route traffic through Tor
+# Set up Tor proxy for Selenium
+proxy = Proxy({
+    'proxyType': ProxyType.MANUAL,
+    'httpProxy': '127.0.0.1:9050',
+    'ftpProxy': '127.0.0.1:9050',
+    'sslProxy': '127.0.0.1:9050',
+})
+
+# Set up Firefox options
+options = FirefoxOptions()
+options.headless = True
+
+# Route traffic through Tor for requests
 SOCKS_PORT = 9050  # Default Tor SOCKS port
 socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", SOCKS_PORT)
 socket.socket = socks.socksocket
@@ -33,19 +49,46 @@ def create_tables():
     ''')
     con.commit()
 
-# Function to scrape URLs from sources
-def scrape_urls(onion_url):
+# Function to get URLs using requests
+def get_urls_with_requests(url):
     try:
-        response = requests.get(onion_url)
-    except requests.exceptions.RequestException as e:
-        print(f"Could not make a request to the url: {onion_url}. Error: {e}")
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        urls = [link.get('href') for link in soup.find_all('a', href=True)]
+        return urls
+    except Exception as e:
+        print(f"Error getting URLs with requests: {e}")
         return []
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-    urls = []
-    for link in soup.find_all('a', href=True):
-        if link['href'].startswith('http'):
-            urls.append(link['href'])
+# Function to get URLs using Selenium
+def get_urls_with_selenium(url):
+    try:
+        # Create a new instance of the Firefox driver
+        driver = webdriver.Firefox(options=options, proxy=proxy)
+
+        # Visit the URL
+        driver.get(url)
+
+        # Wait for the dynamic content to load
+        time.sleep(3)
+
+        # Pass the page source to BeautifulSoup
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+        # Find all 'a' tags with 'href' attributes
+        urls = [link.get('href') for link in soup.find_all('a', href=True)]
+
+        driver.quit()
+        return urls
+    except Exception as e:
+        print(f"Error getting URLs with Selenium: {e}")
+        return []
+
+# Function to scrape URLs from sources
+def scrape_urls(onion_url):
+    urls = get_urls_with_requests(onion_url)
+    if not urls:
+        urls = get_urls_with_selenium(onion_url)
     return urls
 
 # Function to store URLs in a database
@@ -60,22 +103,11 @@ def recursive_crawl(url, depth=0, depth_limit=3):
         return {}
 
     link_mapping = {}
-    try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
+    urls = scrape_urls(url)
+    link_mapping[url] = urls
 
-        outbound_links = []
-        for link in soup.find_all('a', href=True):
-            if link['href'].startswith('http'):
-                outbound_links.append(link['href'])
-
-        link_mapping[url] = outbound_links
-
-        for link in outbound_links:
-            link_mapping.update(recursive_crawl(link, depth+1, depth_limit))
-
-    except requests.exceptions.RequestException as e:
-        print(f"Could not crawl url: {url}. Error: {e}")
+    for link in urls:
+        link_mapping.update(recursive_crawl(link, depth+1, depth_limit))
 
     return link_mapping
 
